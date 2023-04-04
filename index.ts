6bfadb5774683bf70416e2db2acab116a3395b74
Gustavo-Ennes <gustavo@ennes.dev>
2023-04-04 14:39:59 -0300

    AppEngine deploy

--- index.ts
+++ index.ts
@@ -8,24 +8,27 @@ dotenv.config();
 import { server, app } from "./app";
 import "./src/database/database";
 import "./src/firebase/firebase";
+try {
+  (async () => {
+    const httpServer = http.createServer(app);
+    await server.start();
 
-(async () => {
-  const httpServer = http.createServer(app);
-  await server.start();
+    app.use(
+      "/graphql",
+      cors<cors.CorsRequest>(),
+      bodyParser.json(),
+      expressMiddleware(server, {
+        context: async ({ req }) => ({ token: req.headers.token }),
+      })
+    );
 
-  app.use(
-    "/graphql",
-    cors<cors.CorsRequest>(),
-    bodyParser.json(),
-    expressMiddleware(server, {
-      context: async ({ req }) => ({ token: req.headers.token }),
-    })
-  );
-
-  await new Promise<void>((resolve) =>
-    httpServer.listen({ port: 3000 }, resolve)
-  );
-  console.log(
-    `🗺	 ~ Feriapp node graphql backend running at http://localhost:3000/ ~  🗺`
-  );
-})();
+    await new Promise<void>((resolve) =>
+      httpServer.listen({ port: 8080 }, resolve)
+    );
+    console.log(
+      `🗺	 ~ Feriapp node graphql backend running at http://localhost:8080/ ~  🗺`
+    );
+  })();
+} catch (err: any) {
+  console.log("Error at executing the server:\t\t", err);
+}

7cdbcf2feed2369bfe06bea2bd3038a9b8338303
Gustavo-Ennes <gustavo@ennes.dev>
2023-05-05 16:44:29 -0300

    Fix capitalizeName function and tests

---
+++ src/utils/capitalize.test.ts
@@ -0,0 +1,29 @@
+import { describe, it, expect } from "vitest";
+
+import { capitalizeName } from "./capitalize";
+
+const NAMES: string[] = [
+  "sérgio silva dos santos",
+  "antônio de castro",
+  "maria das dores",
+  "lúcia do carmo",
+  "josé da graça",
+  "gustavo ennes",
+];
+const CAPITALIZEDNAMES: string[] = [
+  "Sérgio Silva dos Santos",
+  "Antônio de Castro",
+  "Maria das Dores",
+  "Lúcia do Carmo",
+  "José da Graça",
+  "Gustavo Ennes",
+];
+
+describe("Utils: capitalize text function", async (): Promise<void> => {
+  NAMES.forEach((name: string): void => {
+    it(`name ${name} has to be in CAPITALIZEDNAMES array`, async (): Promise<void> => {
+      const capitalizedName = capitalizeName(name);
+      expect(CAPITALIZEDNAMES).to.include(capitalizedName);
+    });
+  });
+});
--- src/utils/capitalize.ts
+++ src/utils/capitalize.ts
@@ -3,7 +3,7 @@ const inelegibleToTitleWords: string[] = ["da", "das", "do", "dos", "de"];
 const capitalizeName = (name: string) =>
   name
     .split(" ")
-    .map((name) => inelegibleToTitleWords.includes(name) ? name[0] :  name[0].toUpperCase() + name.substring(1))
+    .map((name) => inelegibleToTitleWords.includes(name) ? name :  name[0].toUpperCase() + name.substring(1))
     .join(" ");
 
 export { capitalizeName };

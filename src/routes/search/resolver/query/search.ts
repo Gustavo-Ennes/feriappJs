4c7ca1a12793af681e79faf0d9e1d192ff1d184a
Gustavo-Ennes <gustavo.ennes@3778.care>
2025-01-29 15:53:05 -0300

    Search: error treatment

--- src/routes/search/resolver/query/search.ts
+++ src/routes/search/resolver/query/search.ts
@@ -1,4 +1,5 @@
 import { verifyToken } from "../../../../firebase/firebase";
+import { getLogger } from "../../../../logger/logger";
 import { Department } from "../../../Department";
 import { Vacation } from "../../../Vacation";
 import { VacationInterface } from "../../../Vacation/types/vacation";
@@ -10,32 +11,44 @@ const searchResolver = async (
   _: unknown,
   args: { searchTerm: string },
   context: { token?: string }
-): Promise<SearchResult> => {
-  await verifyToken(context.token || "");
+): Promise<SearchResult | void> => {
+  try {
+    await verifyToken(context.token || "");
 
-  const { searchTerm } = args;
-  const results: SearchResult = {};
-  results.departments = await Department.find({
-    name: { $regex: ".*" + searchTerm + ".*" }
-  });
-  results.workers = await Worker.find({
-    name: { $regex: ".*" + searchTerm + ".*" }
-  })
-    .populate("department")
-    .exec();
-  const allVacations: VacationInterface[] = await Vacation.find({
-    deferred: true
-  })
-    .populate("worker")
-    .exec();
-  results.vacations = [];
-  results.workers.forEach(({ _id }) => {
-    const vacations: VacationInterface[] = allVacations.filter(({ worker }) => {
-      return (worker as WorkerInterface)._id?.toString() === _id?.toString();
+    const { searchTerm } = args;
+    const results: SearchResult = {};
+    results.departments = await Department.find({
+      name: { $regex: ".*" + searchTerm + ".*" }
     });
-    vacations.forEach((vacation) => results.vacations?.push(vacation));
-  });
-  return results;
+    results.workers = await Worker.find({
+      name: { $regex: ".*" + searchTerm + ".*" }
+    })
+      .populate("department")
+      .exec();
+    const allVacations: VacationInterface[] = await Vacation.find({
+      deferred: true
+    })
+      .populate("worker")
+      .exec();
+    results.vacations = [];
+    results.workers.forEach(({ _id }) => {
+      const vacations: VacationInterface[] = allVacations.filter(
+        ({ worker }) => {
+          return (
+            (worker as WorkerInterface)._id?.toString() === _id?.toString()
+          );
+        }
+      );
+      vacations.forEach((vacation) => results.vacations?.push(vacation));
+    });
+    return results;
+  } catch (error) {
+    const logger = getLogger("searchResolver");
+    logger.error(
+      { args },
+      `Erro returning search results: ${(error as Error).message}`
+    );
+  }
 };
 
 export { searchResolver };
